40604d6c2f8aa754fe3a61c5e168497764ae7152
Fedotovskaia Kseniia <sinterma@gmail.com>
2024-07-22 23:57:28 +0200

    corrected mistakes

--- test_search.py
+++ test_search.py
@@ -11,5 +11,7 @@ def test_something(w_size):
 
 def test_something2(w_size):
     browser.open('https://google.com')
+    if browser.element(by.text('Accept All')).matching(be.visible):
+        browser.element(by.text('Accept All')).click()
     browser.element('[name="q"]').should(be.blank).type('tyuudjjj222').press_enter()
     browser.element('[id="botstuff"]').should(have.text('Your search - tyuudjjj222 - did not match any documents.git rm -r --cached .idea'))
