bacccdf3e32ef816a62c45bfc9499d5793402130
Fedotovskaia Kseniia <sinterma@gmail.com>
2024-07-22 23:53:45 +0200

    corrected mistakes

--- conftest.py
+++ conftest.py
@@ -4,10 +4,9 @@ from selene import browser
 
 @pytest.fixture(scope="function")
 def w_size():
+    browser.config.base_url = 'https://google.com'
     browser.config.window_height = 1200
     browser.config.window_width = 1200
-    browser.open('https://google.com')
-
     yield
 
     browser.quit()
--- test_search.py
+++ test_search.py
@@ -1,11 +1,15 @@
-from selene import browser, be, have
+from selene import browser, be, have, by
 
 
 def test_something(w_size):
+    browser.open('https://google.com')
+    if browser.element(by.text('Accept All')).matching(be.visible):
+        browser.element(by.text('Accept All')).click()
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_something2(w_size):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('tyuudjjj222').press_enter()
-    browser.element('[id="botstuff"]').should(have.text('По запросу tyuudjjj222 ничего не найдено. '))
+    browser.element('[id="botstuff"]').should(have.text('Your search - tyuudjjj222 - did not match any documents.'))
